ad3ae4d57ffad540f1ebd74ae680cdd676b447ef
mark <mfc32@pitt.edu>
2025-09-22 14:54:07 -0400

    moved some things around

---
+++ Code/Visuals/Anim.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.AccessControl;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BeeGame;
+
+public class Anim
+{
+
+}
